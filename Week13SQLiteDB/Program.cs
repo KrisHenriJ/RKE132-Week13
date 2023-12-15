ebc793ff05e758b0da93583a5cad8feff47366e1
KrisHenriJ <kris.henri.jarvi@tktk.ee>
2023-12-15 22:29:29 +0200

    week thirteen

---
+++ Week13SQLiteDB/Program.cs
@@ -0,0 +1,14 @@
+﻿using System.Data.SQLite;
+
+SQLiteConnection connection = new SQLiteConnection("Data Source=mydb.db; Version = 3; New = True; Compress = True;");
+try
+{
+    connection.Open();
+    Console.WriteLine("DB dound.");
+}
+catch
+{
+    Console.WriteLine("DB not found.");
+}
+
+connection.Close();
